ea07b07e64975425f835b774474494038facf71a
MarceloALuna <marceloaluna@gmail.com>
2023-09-02 21:39:18 -0300

    se soluciono el cartmodel

--- src/DAO/mongoManager/models/cart.model.js
+++ src/DAO/mongoManager/models/cart.model.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
-const cartModel = new mongoose.Schema({
+const cartcollection = 'cart'
+
+const cartSchema = new mongoose.Schema({
     name: {type: String, required: true},
     image: String,
     price: Number,
     description: String,
     Stock: Number
 })
+
+const cartModel = mongoose.model(cartcollection, cartSchema)
 
 export default cartModel
